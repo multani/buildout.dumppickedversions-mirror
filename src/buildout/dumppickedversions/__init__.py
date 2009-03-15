3ebaf34c76552cd14635a2a1011fde6fff544951
mustapha <mustapha@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-03-15 09:58:26 +0000

    removed the buildout header from the dumped text
    added overwrite-picked-versions-file option
    
    
    git-svn-id: https://svn.plone.org/svn/collective/buildout/buildout.dumppickedversions/trunk@82388 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- src/buildout/dumppickedversions/__init__.py
+++ src/buildout/dumppickedversions/__init__.py
@@ -3,7 +3,7 @@
 # add an attribute in zc.buildout.easy_install.Installer:
 __picked_versions = {} 
 
-# in the last loop in zc.buildout.easy_install.Installer._get_dist method
+# in the last loop in zc.buildout.easy_install.Installer._get_dist method:
 self.__picked_versions[dist.project_name] = dist.version 
 
 #in the end of zc.buildout/buildout.py file
@@ -28,20 +28,29 @@ def enable_dumping_picked_versions(old_get_dist):
     return get_dist
 
 
-def dump_picked_versions(old_logging_shutdown, file_name=None):
+def dump_picked_versions(old_logging_shutdown, file_name, overwrite):
+
     def logging_shutdown():
-        picked_versions = '[buildout]\n\nversions = versions\n\n[versions]\n'
+
+        picked_versions = '[versions]\n'
         for d, v in zc.buildout.easy_install.Installer.__picked_versions.items():
             picked_versions += "%s = %s\n" % (d, v)
+
         if file_name is not None:
-            if os.path.exists(file_name):
-                print "*************************************"
-                print "File %s already exists." % file_name                 
-                print "Please remove it to get a fresh copy"                
-                print "*************************************"
-            else:
+            if not os.path.exists(file_name):
+                print "*********************************************"
                 print "Writing picked versions to %s" % file_name
+                print "*********************************************"
                 open(file_name, 'w').write(picked_versions)
+            elif overwrite:
+                print "*********************************************"
+                print "Overwriting %s" % file_name
+                print "*********************************************"
+                open(file_name, 'w').write(picked_versions)
+            else:    
+                print "*********************************************"
+                print "Skipped: File %s already exists." % file_name                 
+                print "*********************************************"
         else:
             print "*************** PICKED VERSIONS ****************"
             print picked_versions
@@ -51,10 +60,21 @@ def dump_picked_versions(old_logging_shutdown, file_name=None):
 
 
 def install(buildout):
+
     file_name = 'dump-picked-versions-file' in buildout['buildout'] and \
                 buildout['buildout']['dump-picked-versions-file'].strip() or \
                 None
+                
+    overwrite = 'overwrite-picked-versions-file' not in buildout['buildout'] or \
+                buildout['buildout']['overwrite-picked-versions-file'].lower() \
+                in ['yes', 'true', 'on']
+
     zc.buildout.easy_install.Installer.__picked_versions = {}
-    zc.buildout.easy_install.Installer._get_dist = enable_dumping_picked_versions(zc.buildout.easy_install.Installer._get_dist)
-    logging.shutdown = dump_picked_versions(logging.shutdown, file_name)
+    
+    zc.buildout.easy_install.Installer._get_dist = enable_dumping_picked_versions(
+                                  zc.buildout.easy_install.Installer._get_dist)
+    
+    logging.shutdown = dump_picked_versions(logging.shutdown, 
+                                            file_name, 
+                                            overwrite)
     
